104dd94605b6007af243a3c149de0d4520a1e80d
shashankshenvi <shashankshenvi04@gmail.com>
2025-03-23 02:32:11 +0530

    working tree

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 import App from "./App.jsx";
-
-console.log("App is rendering");
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
