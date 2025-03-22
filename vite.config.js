d0a27b4b714ae8789b66c907270aa9afed9e1b86
shashankshenvi <shashankshenvi04@gmail.com>
2025-03-22 19:06:12 +0530

    index.html changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/myPortFoliio/", // This matches your GitHub Pages repository name
+  base: "/myPortFoliio/", // Replace with your actual repo name
   plugins: [react()],
 });
